2eaac68f5f04601c66441d6c109d10c1987c8b8c
Benny Joram <80184074+borvux@users.noreply.github.com>
2025-03-30 07:33:15 +0000

    added before_action to make sure user to access another user's feed/discover

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed discover ]
+  before_action :ensure_current_user, only: [:feed, :discover]
 
   def index
     @users = @q.result
@@ -7,11 +8,17 @@ class UsersController < ApplicationController
 
   private
 
-    def set_user
-      if params[:username]
-        @user = User.find_by!(username: params.fetch(:username))
-      else
-        @user = current_user
-      end
+  def set_user
+    if params[:username]
+      @user = User.find_by!(username: params.fetch(:username))
+    else
+      @user = current_user
     end
+  end
+
+  def ensure_current_user
+    if current_user != @user
+      redirect_back fallback_location: root_url, alert: "You're not authorized for that"
+    end
+  end
 end
