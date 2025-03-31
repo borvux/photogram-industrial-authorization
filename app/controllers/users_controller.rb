c69268b7cb318f3142c2d344ceb5884cc2538e38
Benny Joram <80184074+borvux@users.noreply.github.com>
2025-03-31 02:09:24 +0000

    starting over with pundit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed discover ]
-  before_action :ensure_current_user, only: [:feed, :discover]
 
   def index
     @users = @q.result
@@ -8,17 +7,11 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
-    if params[:username]
-      @user = User.find_by!(username: params.fetch(:username))
-    else
-      @user = current_user
+    def set_user
+      if params[:username]
+        @user = User.find_by!(username: params.fetch(:username))
+      else
+        @user = current_user
+      end
     end
-  end
-
-  def ensure_current_user
-    if current_user != @user
-      redirect_back fallback_location: root_url, alert: "You're not authorized for that"
-    end
-  end
 end
