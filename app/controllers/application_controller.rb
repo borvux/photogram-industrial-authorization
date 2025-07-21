b5d08af1dc91c70db3b5449eacd951fbcd1124c9
Benny Joram <80184074+borvux@users.noreply.github.com>
2025-07-21 22:28:25 +0000

    got rid of the verify authorized and verify policy scoped

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
-  after_action :verify_authorized, except: :index, unless: :devise_controller?
-  after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
-
-  after_action :verify_authorization, unless: :devise_controller?
+  # after_action :verify_authorized, except: :index, unless: :devise_controller?
+  # after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
 
     # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
     allow_browser versions: :modern
@@ -27,14 +25,6 @@ class ApplicationController < ActionController::Base
   end
 
   private
-
-    def verify_authorization
-      if action_name == "index"
-        verify_policy_scoped
-      else
-        verify_authorized
-      end
-    end
 
     def user_not_authorized
       flash[:alert] = "PUNDIT: You are not authorized to perform this action."
